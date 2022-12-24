3b4e1e6a4be3c74c9dd716f19ee7d4ceb273c166
ravsmarty <ravsmarty121@gmail.com>
2022-12-24 11:56:16 +0530

    logger and exception file updated

--- sensor/logger.py
+++ sensor/logger.py
@@ -1,10 +1,10 @@
 import logging
 import os
-from datatime import datatime
+from datetime import datetime
 import os
 
 #log file name
-LOG_FILE_NAME = f"{datatime.now().strftime('%m%d%Y__%H%M_%S')}.log"
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
 #log directory
 LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
@@ -14,10 +14,10 @@ os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
 #log file path
 
-LOG_FILE_PATH = os.path.join(LOG_FILE_DIR, LOG_FILE_NAME)
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 
 logging.basicConfig(
-    filename=LOG_FILE_NAME,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(messsage)s",
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
