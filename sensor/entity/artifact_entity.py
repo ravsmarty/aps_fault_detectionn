8a95e0c3a1f2663cea413ab825c06561512e06ef
ravsmarty <ravsmarty121@gmail.com>
2023-01-04 11:46:41 +0530

    initial part

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -12,14 +12,20 @@ class DataValidationArtifact:
     report_file_path:str
 
 @dataclass    
-class DataTransformationArifact:
+class DataTransformationArtifact:
     transform_object_path:str 
     transformed_train_path:str
     transformed_test_path:str
     target_encoder_path:str
     
 
-    
-class ModelTrainerArifact:...
+@dataclass  
+class ModelTrainerArtifact:
+     model_path:str 
+     f1_train_score:float 
+     f1_test_score:float
+
+
+
 class ModelEvaluationArifact:...
 class ModelPusherArifact:...
